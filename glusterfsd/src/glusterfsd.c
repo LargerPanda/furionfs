04de92a118d6f576e9e8ac9556e5cc0f0f835adc
Be_a_panda <huayusheng0912@gmail.com>
2021-10-19 10:55:51 +0800

    debug

--- glusterfsd/src/glusterfsd.c
+++ glusterfsd/src/glusterfsd.c
@@ -2735,6 +2735,11 @@ app_info_t* new_app_info(){
 int
 main(int argc, char *argv[])
 {
+    if(argc!=2){
+        print("wrong input parameters!\n");
+        print("usage: glusterfs #num_io\n");
+    }
+    
     //先生成app_info，指定app的rpl标签，并且初始化时间戳
     app_info_t *global_app_info = new_app_info();
 
